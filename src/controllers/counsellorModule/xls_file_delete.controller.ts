166a60f098d40a3c47c54f5a2f67310c375a1856
inventog-official <inventogofficial@gmail.com>
2025-04-03 17:00:52 +0530

    xls delete api pending

---
+++ src/controllers/counsellorModule/xls_file_delete.controller.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Response } from "express";
+import { AuthRequest } from "../../middlewares/auth.middleware";
+import { AppError } from "../../utils/errorHandler";
+import { xlsFileUploadSchema } from "../../zodSchema/counsellor.schema";
+import { updateXlsFileNameService } from "../../services/counsellorModule/xls_fileName_update.service";
+
+export const deleteXlsFileController = async (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+        // Validate user authentication
+        if (!req.user) {
+            throw new AppError({ statusCode: 401, message: "Unauthorized access", data: {} });
+        }
+
+        if (!req.query.xls_file_id) {
+            throw new AppError({ statusCode: 400, message: "xls_file_id is required", data: {} });
+        }
+
+        // Validate Request Body
+        const validatedData = xlsFileUploadSchema.safeParse(req.body);
+
+        if (!validatedData.success) {
+            throw new AppError({
+                statusCode: 400,
+                data: {},
+                message: validatedData.error.errors[0].message, // Get first error message
+            });
+        }
+
+        const { fileName: newFileName } = validatedData.data;
+
+        // Call Service to update the xls file name
+        const { responseUpdateXlsFileName } = await updateXlsFileNameService({
+            newFileName: newFileName,
+            xls_file_id: req.query.xls_file_id as string,
+            management_staff_id: req.user.id
+        });
+
+        res.status(201).json({
+            status: true,
+            data: responseUpdateXlsFileName,
+            message: "XLS file name updated successfully",
+        });
+    } catch (error) {
+        console.error("Error in update xls File name Controller:", error);
+        next(error);
+    }
+
+}
