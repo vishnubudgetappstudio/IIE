b3a66fab7da86351791467c93cb2907a43bc372b
inventog-official <inventogofficial@gmail.com>
2025-04-10 16:20:41 +0530

    bug

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getProfileDetailsController } from "../controllers/profile/get_profile_details.controller";
 import { updateProfileDetailsController } from "../controllers/profile/update_profile_details.controller";
+import { upload } from "../middlewares/upload.middleware";
 
 
 const router = Router();
 
 router.get("/get", getProfileDetailsController); // get Profile route
-router.post("/update", updateProfileDetailsController); // update Profile route
+router.post("/update", upload.single("image"), updateProfileDetailsController); // update Profile route
 
 export default router;
