3af8087d4f844d8ad513513d55475f6e688947fb
vishnubudgetappstudio <142775315+vishnubudgetappstudio@users.noreply.github.com>
2025-05-05 11:32:34 +0530

    again err fix router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -78,8 +78,8 @@ router.get("/get-mock-test-questions", getMockTestQuestionsController);
 //Staff API routes
 router.get("/get-batch-details", getStaffBatchesListController);
 router.get("/get-batch-students", getAllStudentsFromBatchController); // Get Batch Details route
-router.get("/get-session-sheet", getSessionSheetDataController);
-router.post("/update-session-sheet-status", updateSessionSheetStatusController); // Get Batches List route
+//router.get("/get-session-sheet", getSessionSheetDataController);
+//router.post("/update-session-sheet-status", updateSessionSheetStatusController); // Get Batches List route
 router.post("/attendance/mark", markStudentAttendanceController);
 router.get("/attendance/percentage", getStudentAttendanceStatsController);
 router.post("/upload-pdfMaterial-students", uploadPDFMaterialFileController);
