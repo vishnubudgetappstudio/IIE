229497862fef2446759cd003800242281b5c7f01
vishnubudgetappstudio <142775315+vishnubudgetappstudio@users.noreply.github.com>
2025-05-17 10:42:44 +0530

    auth route file move

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,6 +5,8 @@ import {
   login,
   signup,
   verifyOTPController,
+  sendGuestOtp,
+  verifyGuestOtp
 } from "../controllers/auth.controller";
 import { upload } from "../middlewares/upload.middleware";
 
@@ -15,5 +17,26 @@ router.post("/login", upload.none(), login);
 router.post("/forgot-password", upload.none(), forgotPasswordManagementStaffController);
 router.post("/verify-otp", upload.none(), verifyOTPController);
 router.post("/reset-password", upload.none(), resetPasswordManagementStaffController);
+
+
+// ✅ Guest login routes
+//router.post("/guest/signup", upload.none(), sendGuestOtp);
+router.post("/guest/signup", upload.none(), async (req, res, next) => {
+  try {
+    
+    await sendGuestOtp(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+router.post("/guest/verify-otp", upload.none(), async (req, res, next) => {
+  try {
+    await verifyGuestOtp(req, res);
+  } catch (error) {
+    next(error);
+  }
+});//router.post('/auth/guest/send-otp', sendGuestOtp);
+//router.post('/auth/guest/verify-otp', verifyGuestOtp);
+
 
 export default router;
