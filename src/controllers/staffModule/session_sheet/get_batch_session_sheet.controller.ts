7c4473a85d69ff50bfc779dab2ab369ea3645d7f
vishnubudgetappstudio <142775315+vishnubudgetappstudio@users.noreply.github.com>
2025-05-21 16:31:49 +0530

    move mock test count.

--- src/controllers/staffModule/session_sheet/get_batch_session_sheet.controller.ts
+++ src/controllers/staffModule/session_sheet/get_batch_session_sheet.controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from "express";
 import { AuthRequest } from "../../../middlewares/auth.middleware";
 import { AppError } from "../../../utils/errorHandler";
 import { getSessionSheetDataService } from "../../../services/counsellorModule/batch_related/get_session_sheet_data.service";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, SessionSheetStatus } from '@prisma/client';
 
 const prisma = new PrismaClient();
 /**
@@ -55,25 +55,67 @@ export const getSessionSheetReport = async (req: AuthRequest, res: Response, nex
   const offset = (page - 1) * limit;
 
   try {
-    const records = await prisma.$queryRawUnsafe<any[]>(`
-      SELECT 
-        sssrd.id,
-        sssrd.student_id,
-        sssrd.status,
-        s.name AS student_name,
-        s.email AS student_email,
-        s.roll_number AS roll_number
-      FROM session_sheet_student_report_detail sssrd
-      LEFT JOIN student s ON sssrd.student_id = s.id
-      ORDER BY sssrd.id
-      LIMIT ${limit} OFFSET ${offset};
-    `);
+    // const records = await prisma.$queryRawUnsafe<any[]>(`
+    //   SELECT 
+    //     sssrd.id,
+    //     sssrd.student_id,
+    //     sssrd.status,
+    //     s.name AS student_name,
+    //     s.email AS student_email,
+    //     s.roll_number AS roll_number
+    //   FROM session_sheet_student_report_detail sssrd
+    //   LEFT JOIN student s ON sssrd.student_id = s.id
+    //   ORDER BY sssrd.id
+    //   LIMIT ${limit} OFFSET ${offset};
+    // `);
 
-    const totalResult = await prisma.$queryRawUnsafe<any[]>(`
-      SELECT COUNT(*) AS count FROM session_sheet_student_report_detail;
-    `);
+    // const totalResult = await prisma.$queryRawUnsafe<any[]>(`
+    //   SELECT COUNT(*) AS count FROM session_sheet_student_report_detail;
+    // `);
 
-    const totalRecords = parseInt(totalResult[0].count, 10);
+    const STATUS_LABELS: Record<SessionSheetStatus, string> = {
+      Completed: 'Completed',
+      HaveDoubt: 'Have doubt',
+      Incomplete: 'inComplete',
+      NotMarked: 'Not Marked',
+    };
+
+    const transformedRecords = await prisma.sessionSheetStudentReportDetail.findMany({
+      where: {
+        session_sheet_id: req.query.session_sheet_id as string,
+        deletedAt: null,
+      },
+      select: {
+        id: true,
+        student_id: true,
+        status: true,
+        student: {
+          select: {
+            name: true,
+            profile_img_url: true,
+            roll_number: true,
+          },
+        },
+      }
+    });
+
+    const records = transformedRecords.map((record) => ({
+      // ...record,
+      id: record.id,
+      student_id: record.student_id,
+      student_name: record.student?.name,
+      student_profile: record.student?.profile_img_url,
+      roll_number: record.student?.roll_number,
+      status: STATUS_LABELS[record.status], // ✅ user-friendly display
+    }));
+
+    // const totalRecords = parseInt(records[0].count, 10);
+    const totalRecords = await prisma.sessionSheetStudentReportDetail.count({
+      where: {
+        session_sheet_id: req.query.session_sheet_id as string,
+        deletedAt: null,
+      }
+    });
     const totalPages = Math.ceil(totalRecords / limit);
 
     res.json({
