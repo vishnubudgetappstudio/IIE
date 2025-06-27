388a560231e434c420d56d3136856f4fcf09a2d0
vishnubudgetappstudio <142775315+vishnubudgetappstudio@users.noreply.github.com>
2025-06-27 18:53:44 +0530

    move modified files.

--- src/controllers/staffModule/approveStudentLeaveController.ts
+++ src/controllers/staffModule/approveStudentLeaveController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Response } from "express";
 import { AuthRequest } from "../../middlewares/auth.middleware";
 import { PrismaClient } from '@prisma/client';
+import { AppError } from "../../utils/errorHandler"; // Adjust the path if AppError is located elsewhere
 
 const prisma = new PrismaClient();
 
@@ -24,19 +25,58 @@ export const approveStudentLeaveController = async (
     }
 
     const statusUpdate = await prisma.leaveDetail.update({
-  where: { id: leaveDetailId },
-  data: {
-    status,
-    notificationRecipients: {
-      updateMany: {
-        where: { leaveDetailId: leaveDetailId },
-        data: {
-          isRead: true,
+      where: { id: leaveDetailId },
+      data: {
+        status,
+        notificationRecipients: {
+          updateMany: {
+            where: { leaveDetailId: leaveDetailId },
+            data: {
+              isRead: true,
+            },
+          },
         },
       },
-    },
-  },
-});
+    });
+
+    if (!statusUpdate.student_id) {
+      res.status(400).json({ message: "Student ID is missing in leave detail." });
+      return;
+    }
+    const student = await prisma.student.findUnique({
+      where: { id: statusUpdate.student_id },
+      select: { fcm_token: true, name: true },
+    });
+
+    const fcmToken = student?.fcm_token;
+    
+          const message = {
+              notification: {
+                  title: 'Leave Request Update',
+                  body: `Your leave request ${status}.`,
+              },
+              token: fcmToken as string,
+          };
+    
+          await prisma.notificationRecipient.create({
+            data: {
+              title: message.notification.title,
+              description: message.notification.body,
+              receiverRole: 'student',
+              type: 'message',
+              isRead: false,
+              status: 'Sent',
+              studentId: statusUpdate.student_id,
+            },
+          });
+      
+          if (!student || !student.fcm_token) {
+              throw new AppError({
+                  statusCode: 404,
+                  message: "Student FCM token not found",
+                  data: {},
+              });
+          }
 
 
     res.status(200).json({
